3accfa5f47900615af0b8c151c822c1acc63d3f6
XinaKiller <pdamiao9@gmail.com>
2022-02-28 17:41:22 +0000

    Up-to 2.2 of TP1: followed TDD, writing all tests before implementing any of the methods in Account. All tests passed on first try, with 100% coverage. For some reason the tests only run on Junit 5.6.0, probably poorly setup. The .gitignore file was generated using toptal's site.

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,2 +1,55 @@
 public class Account {
+
+    private String name;
+    private int age;
+    private float balance;
+    private int nr_children;
+
+
+    public Account(String name, int age, float balance, int nr_children) {
+        this.name = name;
+        this.age = age;
+        this.balance = balance;
+        this.nr_children = nr_children;
+    }
+
+    public String getClientName() {
+        return this.name;
+    }
+
+    public int getClientAge() {
+        return this.age;
+    }
+
+    public float getClientBalance() {
+        return this.balance;
+    }
+
+    public int getClientNrChildren() {
+        return this.nr_children;
+    }
+
+    public void setClientName(String new_name) {
+        this.name = new_name;
+    }
+
+    public void setClientAge(int new_age) {
+        this.age = new_age;
+    }
+
+    public void setClientBalance(float new_balance) {
+        this.balance = new_balance;
+    }
+
+    public void setClientNrChildren(int new_nrchildren) {
+        this.nr_children = new_nrchildren;
+    }
+
+    public void deposit(float amount) {
+        this.balance += amount;
+    }
+
+    public void withdraw(float amount) {
+        this.balance -= amount;
+    }
 }
